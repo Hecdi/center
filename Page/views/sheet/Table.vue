8c776da9e60a6de82375a06c93778d3be6e9edff
hecdi <1652777847@qq.com>
2018-11-28 16:25:41 +0800

    报表页面样式修改

--- Page/views/sheet/Table.vue
+++ Page/views/sheet/Table.vue
@@ -6,17 +6,68 @@
       style="width: 100%">
       <el-table-column
         prop="date"
-        label="日期"
-        width="180">
+        label="机型"
+        width="80">
       </el-table-column>
       <el-table-column
         prop="name"
-        label="姓名"
-        width="180">
+        label="机类"
+        width="80">
       </el-table-column>
       <el-table-column
         prop="address"
-        label="地址">
+        label="航线"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="预达"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="计达"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="实达"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="前飞"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="登机"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="协关"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="计飞"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="实飞"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="引导车"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="引导人员"
+        width="120">
       </el-table-column>
     </el-table>
   </div>
@@ -27,21 +78,21 @@
     data() {
       return {
         tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          date: '2016',
+          name: '王虎',
+          address: '上海'
         }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
+          date: '05-04',
+          name: '小虎',
+          address: '普陀区'
         }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
+          date: '2016',
+          name: '王小',
+          address: '金沙江路'
         }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          date: '2013',
+          name: '王虎',
+          address: '1516'
         }]
       }
     }
