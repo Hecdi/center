7f987ba839767d20f54c06856df0d4dac63adab3
hecdi <1652777847@qq.com>
2018-11-30 10:25:21 +0800

    违规记录单位管理弹出框的添加

--- Page/views/violation/TopBar.vue
+++ Page/views/violation/TopBar.vue
@@ -20,7 +20,7 @@
                     </el-form-item>
                 </el-form> -->
             </el-col>
-            <el-col :span="16">
+            <el-col :span="16" class="topbar">
                 <el-form ref="form" label-width="80px">
                     <el-form-item :model="formInline" >
                         <el-select v-model="formInline.region" placeholder="活动区域">
@@ -51,13 +51,50 @@
                 <!-- </div> -->
             </el-col>
             <el-col :span="3">
-                <el-button>单位管理</el-button>
+                <el-button @click="dialogVisible = true">单位管理</el-button>
                 <el-button>导出</el-button>
             </el-col>
+            <div class="dialog">
+            <el-dialog
+                title="单位管理"
+                :visible.sync="dialogVisible"
+                width="30%"
+                :before-close="handleClose">
+                <span>这是一段信息</span>
+                <el-form ref="form" :model="form" label-width="80px">
+                    <el-form-item label="单位编号">
+                      <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="单位编号">
+                      <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="单位编号">
+                      <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                  <el-button @click="dialogVisible = false">取 消</el-button>
+                  <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                </span>
+            </el-dialog>
+            </div>
         </el-row>
     </div>
 </template>
 
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+    methods: {
+      
+    }
+  };
+</script>
 <script>
       export default {
         data() {
@@ -74,12 +111,23 @@
             },
             inputSearch:'',
             time: '',
+            dialogVisible: false,
+            form: {
+                name: 'placeholder',
+            },
           }
         },
         methods: {
-          onSubmit() {
-            console.log('submit!');
-          }
-        }
-    } 
+            onSubmit() {
+              console.log('submit!');
+            },
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                      done();
+                    })
+                    .catch(_ => {});
+                }
+            }
+        } 
 </script>
