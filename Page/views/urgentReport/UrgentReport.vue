1476221cf1d2c1622b4c806f86c61d68ec203a61
wanli <190231105@qq.com>
2018-12-11 15:34:05 +0800

    偏离上报新需求全部页面

--- Page/views/urgentReport/UrgentReport.vue
+++ Page/views/urgentReport/UrgentReport.vue
@@ -1,5 +1,24 @@
 <template>
-	<section class="urgentReport" >
+<section class="outermostsection" >
+	<section class="outermostheader">
+		<el-row :gutter="20">
+			<el-col :span="12">
+				<el-radio-group v-model="radio1">
+					<el-radio-button round label="当前部门" ></el-radio-button>
+					<el-radio-button round label="所有部门" ></el-radio-button>
+				</el-radio-group>
+			</el-col>
+			<el-col :span="6">
+				<el-input class="input1" placeholder="选择时间段" prefix-icon="el-icon-date" v-model="input1" clearable>
+				</el-input>
+			</el-col>
+			<el-col :span="6">
+				<el-input placeholder="输入关键词搜索" suffix-icon="el-icon-search" v-model="input2" clearable>
+				</el-input>
+			</el-col>
+		</el-row>
+	</section>
+	<section class="urgentReport" v-if="radio1=='当前部门'">
 		<el-row :gutter="5" id='card'>
 			<el-col :span="4" v-for="site in sites" :key="site.flightTaskId" >
 				<el-card>
@@ -22,12 +41,12 @@
 								<el-button type="text" @click="dialogVisible = true">
 									图片详情>
 								</el-button>
-								<el-dialog 
+								<el-dialog
 									title="提示"
 									:visible.sync="dialogVisible">
 									<el-carousel height="150px">
-										<el-carousel-item  :showImg="site.imgFile">
-											<img :src="showImg" />
+										<el-carousel-item  >
+											<img />
 										</el-carousel-item>
 									</el-carousel>
 								</el-dialog>
@@ -38,6 +57,53 @@
 				</el-col>	
 			</el-row>
 	</section>
+	<section v-else>
+		<el-table 
+			:data = "sites"
+			stripe
+			highlight-current-row
+			style = "width:auto">
+			<el-table-column 
+				type = "index"
+				width = "80px"
+				label = "序号">
+			</el-table-column>
+			<el-table-column
+				prop = "flightNo"
+				width = "180px"
+				label = "航班号">
+			</el-table-column>
+			<el-table-column
+				prop = "deviationItemName"
+				width = "180px"
+				label = "步骤名称">
+			</el-table-column>
+			<el-table-column
+				prop = "staffName"
+				width = "180px"
+				label = "上报人">
+			</el-table-column>
+			<el-table-column
+				prop = "deviateTime"
+				min-width = "180px"
+				label = "上报时间">
+			</el-table-column>
+			<el-table-column
+				prop = "remarks"
+				min-width = "300px"
+				label = "偏离描述">
+			</el-table-column>
+			<el-table-column label = "附件">
+				<template slot-scope="scope" >
+					<div v-if = "scope.row.imgFile">
+						<el-button type = "primary" @click="dialogVisible = true">查看</el-button>
+					</div>
+					<div v-else>查看</div>
+				</template>
+			</el-table-column>
+		</el-table>
+	</section>
+</section>
 </template>
 
 
@@ -50,7 +116,10 @@ import {formatDate} from "date.js";
 			return {
 				sites:[],
 				dialogVisible:false,
-				showImg: '',
+				radio1:'当前部门',
+				input1:'',
+				input2:'',
+				
 			}
 		},
 		methods:{
@@ -82,16 +151,15 @@ import {formatDate} from "date.js";
 					  /*'flightNo':'EU2280',*/
 					  /*'staffName':'leo',*/
 					  /*'deviateTime':1544075290,*/
-					  /*'conTent':'这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长',*/
-					  /*'img':'aaaaaasbbb',*/
+					  /*'content':'这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长这是很长很长的内容很长很长',*/
+					  /*'imgFile':'aaaaaasbbb',*/
 					 /*},*/
-					 /*{'deviationItemName':'撤桥',*/
-					  /*'remarks':'航后',*/
+					/*{'deviationItemName':'撤桥',*/
+					  /*'remarks':'这是很长很长的内容很长很长这是很长很长的内容很长很长',*/
 					  /*'flightNo':'EU2280',*/
 					  /*'staffName':'leo',*/
 					  /*'deviateTime':1544075290,*/
 					  /*'conTent':'这是很长很长的内容很长很长这是很长很长的内容很长很长'*/
-
 					 /*}*/
 				/*];*/
 				/*this.getData(data);*/
