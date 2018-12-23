7ddeb66b7746c5bbf2392ef84531c37a0d163fd4
hecdi <1652777847@qq.com>
2018-12-23 15:36:08 +0800

    导航栏样式的修改

--- Page/App.vue
+++ Page/App.vue
@@ -3,8 +3,9 @@
     <el-container>
       <el-header height="50px;">
         <div id="nav" class="top-menu navbar navbar-dark bg-inverse">
-          <a class="navbar-brand" href="javascript:void(0)"/>
-          <span class="navbar-departName">机坪运行管理</span>
+          <span class="navbar-brand" href="javascript:void(0)">
+            <span class="nav-title">机坪运行管理</span></span>
+          <!-- <span class="navbar-departName">机坪运行管理</span> -->
           <ul class="nav navbar-nav">
             <router-link tag="li" to="/home" class="nav-item" exact>排班</router-link>
             <router-link tag="li" to="/rollCall" class="nav-item">点到</router-link>
@@ -18,7 +19,7 @@
               <!--[> 00 <]-->
             <!--</router-link>-->
           </ul>
-		  <el-button size="mini" style="float:right;margin-top:12px;" type="info" @click="logOut">登出</el-button>
+		    <el-button size="mini" style="float:right;" type="info" @click="logOut">登出</el-button>
         </div>
       </el-header>
       <el-container>
