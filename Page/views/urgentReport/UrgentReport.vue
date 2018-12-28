85895cf3ef6ba2e115ba6f94d659593ac36d2446
zhanghan <cathacker2008@163.com>
2018-12-29 06:48:00 +0800

    偏离上报隐藏及图片样式调整

--- Page/views/urgentReport/UrgentReport.vue
+++ Page/views/urgentReport/UrgentReport.vue
@@ -62,7 +62,7 @@
 								<el-dialog
 									title="提示"
 									:visible.sync="dialogVisible">
-									<el-carousel :autoplay=false  height="600px" >
+									<el-carousel :autoplay=false  height="900px" >
 										<el-carousel-item  v-for = "(item,index) in imgArr" :key = "index">
 											<img :src= "item"  class="img" />
 										</el-carousel-item>
@@ -139,7 +139,7 @@
 							<div>
 								<el-button style="padding:8px 16px" :type = "scope.row.imgFile  && scope.row.imgFile != '{}'?'primary':'info'" @click.native="showPics(scope.row.imgFile)">查看</el-button>
 								<el-dialog v-if = "scope.row.imgFile && scope.row.imgFile != '{}'"  title="提示" :visible.sync="dialogVisible">
-									<el-carousel :autoplay=false height="600px" >
+									<el-carousel :autoplay=false height="900px" >
 										<el-carousel-item v-for = "(item,index) in imgArr" :key = "index">
 											<img :src="item" class="img"/>
 										</el-carousel-item>
