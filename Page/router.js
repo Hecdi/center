02d32bea34632db63b3706af92ff602d1fc2a20e
zhanghan <cathacker2008@163.com>
2018-12-28 17:24:58 +0800

    “操作手册及首页弹窗”

--- Page/router.js
+++ Page/router.js
@@ -7,6 +7,7 @@ import Violation from "./views/violation/ViolationRecord.vue";
 import UrgentReport from "./views/urgentReport/UrgentReport.vue";
 import MessageCenter from "./views/messageCenter/MessageCenter.vue";
 import Statistics from "./views/statistics/Statistics.vue";
+import Manual from "./views/manual/Manual.vue";
 import Login from "./views/login/Login.vue";
 Vue.use(Router);
 
@@ -77,6 +78,15 @@ export const smartSchedulingRouter = new Router({
 			path: "/messageCenter",
 			name: "messageCenter",
 			component:MessageCenter,
+			meta: {
+				title: '',
+				requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
+			}
+		},
+		{
+			path: "/operationManual",
+			name: "operationManual",
+			component:Manual,
 			meta: {
 				title: '',
 				requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
