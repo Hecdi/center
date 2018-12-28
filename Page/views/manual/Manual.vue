236d8c2c62e1b6ec02cda2ddf19adcc3d1a40f53
zhanghan <cathacker2008@163.com>
2018-12-28 17:24:09 +0800

    “操作手册及首页弹窗”

---
+++ Page/views/manual/Manual.vue
@@ -0,0 +1,29 @@
+<template>
+	<section class="manual" >
+		<img :src="`../../../public/manual/${index}.png`" v-for="index in imgs" :key="index"/>
+	</section>
+</template>
+
+<script>
+	// @ is an alias to /src
+	export default {
+		name: "Manual",
+		data() {
+			return {
+				imgs:[1,2,3,4,5,6,7,8,9],
+			};
+		},
+		methods: {
+		},
+		computed: {
+		},
+		components: {
+			/*SearchInput,*/
+		},
+		beforeMount() {
+		},
+		beforeDestroy() {
+		}
+	};
+</script>
+
