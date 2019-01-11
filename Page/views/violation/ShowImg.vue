b33c916097f6dfb9cc9469b074ceff1cb7b3c6b6
hecdi <1652777847@qq.com>
2019-01-11 17:13:51 +0800

    违规记录图片轮播高度自适应

--- Page/views/violation/ShowImg.vue
+++ Page/views/violation/ShowImg.vue
@@ -3,16 +3,12 @@
         :visible.sync="showImgDialog"
         width="550px"
        >
-    <el-carousel indicator-position="outside" :height="height" :interval=3000 @change="changeItem">
+    <el-carousel indicator-position="outside" :height="height" :interval=2000 @change="changeItem">
       <el-carousel-item v-for="(item, index) in img" :key="item+index">
-        <img  ref="item" :src ="item" style="width:100%;height:`${height}`;"/>
+        <img  :src ="item" style="width:100%;height:`${height}`;"/>
       </el-carousel-item>
     </el-carousel>
-    <span slot="footer" class="dialog-footer" style="display:none;">
-      <el-button type="primary" @click="showImgDialog = false">关闭</el-button>
-    </span>
 </el-dialog>
-
 </template>
 
 <script>
@@ -27,19 +23,6 @@
           visibleSync (val) {
             this.$emit('update:dialogVisible', val)
           },
-          // showImgDialog(now,old){
-          //   let that = this;
-          //   that.imgHeight = '620px'
-          //   window.onresize = function temp() {
-          //       // 通过点语法获取img的height属性值
-          //       console.log(that.$refs.imgHeight['0']);
-          //       that.$nextTick(() => {
-          //         that.imgHeight = `${that.$refs.imgHeight['0'].height}px`;
-          //       })
-             
-          //    console.log(that.imgHeight);
-          //   }
-          // },
         },
         methods: {
           handleChangeVisible(){ 
@@ -51,25 +34,21 @@
             this.everyImg = this.value;
           },
           getHeight(active) {
-            // this.$nextTick(() => {
-            //   this.height = this.$refs.item[this.active].clientHeight + 'px'
-            // })
             let img = new Image();
             let that = this;
+            img.src = this.img[active];
             img.onload = function(){
-              console.log(this);
               let ration = 550/this.width;
               that.height = this.height>300?this.height*ration+'px':'300px';
             }
-            console.log(that.height);
-            img.src = this.img[active];
           },
           changeItem(active, pre) {
             this.active = active
             this.getHeight(active);
           }
-  
-          
+        },
+         mounted() {
+          this.getHeight()
         },
         data () {
           return {
@@ -80,9 +59,7 @@
             height: '',
           }
         },
-        mounted() {
-          this.getHeight()
-        },
+       
         computed:{
             ...mapState('violation', ['showImgDialog']),
           showImgDialog: {
