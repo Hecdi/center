fc1320b00f4d9ce017f008e45a54d6c3d3b2f51b
hecdi <1652777847@qq.com>
2019-01-11 16:34:16 +0800

    违规记录图片轮播高度自适应

--- Page/views/violation/ShowImg.vue
+++ Page/views/violation/ShowImg.vue
@@ -1,14 +1,14 @@
 <template>
     <el-dialog
         :visible.sync="showImgDialog"
-        width="400px"
+        width="550px"
        >
-    <el-carousel indicator-position="outside" height="600px">
-    <el-carousel-item v-for="(item, index) in img" :key="item+index">
-      <img :src ="item" style="width:100%;height:auto;"/>
-    </el-carousel-item>
-  </el-carousel>
-    <span slot="footer" class="dialog-footer">
+    <el-carousel indicator-position="outside" :height="height" :interval=3000 @change="changeItem">
+      <el-carousel-item v-for="(item, index) in img" :key="item+index">
+        <img  ref="item" :src ="item" style="width:100%;height:`${height}`;"/>
+      </el-carousel-item>
+    </el-carousel>
+    <span slot="footer" class="dialog-footer" style="display:none;">
       <el-button type="primary" @click="showImgDialog = false">关闭</el-button>
     </span>
 </el-dialog>
@@ -26,7 +26,20 @@
         watch: {
           visibleSync (val) {
             this.$emit('update:dialogVisible', val)
-          }
+          },
+          // showImgDialog(now,old){
+          //   let that = this;
+          //   that.imgHeight = '620px'
+          //   window.onresize = function temp() {
+          //       // 通过点语法获取img的height属性值
+          //       console.log(that.$refs.imgHeight['0']);
+          //       that.$nextTick(() => {
+          //         that.imgHeight = `${that.$refs.imgHeight['0'].height}px`;
+          //       })
+             
+          //    console.log(that.imgHeight);
+          //   }
+          // },
         },
         methods: {
           handleChangeVisible(){ 
@@ -36,14 +49,39 @@
           },
           getEveryImg(value){
             this.everyImg = this.value;
+          },
+          getHeight(active) {
+            // this.$nextTick(() => {
+            //   this.height = this.$refs.item[this.active].clientHeight + 'px'
+            // })
+            let img = new Image();
+            let that = this;
+            img.onload = function(){
+              console.log(this);
+              let ration = 550/this.width;
+              that.height = this.height>300?this.height*ration+'px':'300px';
+            }
+            console.log(that.height);
+            img.src = this.img[active];
+          },
+          changeItem(active, pre) {
+            this.active = active
+            this.getHeight(active);
           }
+  
+          
         },
         data () {
           return {
             visibleSync: this.dialogVisible,
             modelImg: [img,img,img],
             picUrl: [],
+            imgHeight: '500',
+            height: '',
           }
+        },
+        mounted() {
+          this.getHeight()
         },
         computed:{
             ...mapState('violation', ['showImgDialog']),
@@ -53,8 +91,9 @@
           },
           set() {
             this.$store.dispatch(`violation/updateShowImg`, { showImgDialog: false });
-          }
+          },
         },
+        
         img:function(){
           let arr=[];
           each(this.picture,(item) => {
